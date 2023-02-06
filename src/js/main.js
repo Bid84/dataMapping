f3bdfcd553a0751c1e83f773a8f3478f29804f69
Bidur <thapabidur86@gmail.com>
2023-02-06 20:06:42 +0100

    data mapped

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,51 @@
-function allowDrop(ev){
-  ev.preventDefault();
+
+let csvArray = [
+     {path:"./source.csv", list:"list0"},
+     {path:"./target.csv", list:"list1"}
+
+]
+for (let i=0; i<csvArray.length;i++){
+  fetch(csvArray[i].path).then(res=>{
+    return res.text()
+ }).then(data => {
+   let result =data.split(/\r?\n|\r/).map(
+     e=>{
+       return e.split(",")
+     }
+   )
+  
+   result.forEach( e =>{
+     let m = e.map(e =>{
+      return `<li class="list-group-item"> ${e} </li>`;
+     }).join(" ");
+     let list = csvArray[i].list;
+     let myUl = document.querySelector("#" +list);
+     myUl.innerHTML =m;
+   
+ 
+   })
+ })
 }
 
-function drag(ev)
-{
-    ev.dataTransfer.setData("text", ev.target.id);
-      var data = ev.dataTransfer.getData("text");
-      ev.target.appendChild(document.getElementById(data));
-
-
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
-
-
-
-/*
-<script>
-  var originalData = document.getElementById("original-data");
-  originalData.addEventListener("dragstart", function(event) {
-    var copiedData = originalData.cloneNode(true);
-    var newElement = document.createElement("div");
-    newElement.appendChild(copiedData);
-    newElement.innerHTML = copiedData.innerHTML;
+ $(function() {
+  $( "#list0" ).draggable({
+    revert: true,
+    opacity: 0.40
   });
-</script>*/
+}); 
 
+$(document).ready(function (){
+  $("#list0").selectable();
+});
+
+$(function(){
+  $( "#list1" ).droppable();
+   
+});
+
+$(document).ready(function(){
+  $("button").click(function(){
+
+  })
+})
 
